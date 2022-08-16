5e09aa7d513c8cf1a1312a1486040ea9767099a5
akashsushi <69210145+akashsushi@users.noreply.github.com>
2022-08-16 12:13:00 +0530

    Add files via upload

---
+++ BiggestOfThree.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class BiggestOfThree
+    {
+        public void Main()
+        {
+            int number1, number2, number3;
+
+            Console.WriteLine("Enter the first value");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second value");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the third value");
+            number3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The biggest value is "+Biggest(number1,number2,number3));
+
+            Console.WriteLine("Now using Ternary operator");
+
+            Console.WriteLine("The biggest value is " + BiggestUsingTernary(number1, number2, number3));
+        }
+
+        int Biggest(int num1, int num2, int num3)
+        {
+            int big = num1;
+
+            if(big < num2) 
+            {
+                if(num2 > num3)
+                {
+                    big = num2;
+                }
+                else
+                {
+                    big = num3;
+                }
+            }
+        
+            return big;
+        }
+
+        int BiggestUsingTernary(int num1, int num2, int num3)
+        {
+            int big = 0;
+             big = num1 > num2 ? num1 : num2;
+             big = big > num3 ? big : num3;
+            return big;
+        }
+
+    }
+}
---
+++ SwappingOfTwo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class SwappingOfTwo
+    {
+        public void Main()
+        {
+            int number1, number2;
+
+            Console.WriteLine("Enter the first value");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second value");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"The numbers are  number1 = {number1} and number2 ={number2}");
+
+            Swapping(number1, number2);
+        }
+
+        void Swapping(int num1, int num2)
+        {
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            num1 = num1 - num2;
+
+            Console.WriteLine($"The swapped numbers are  number1 = {num1} and number2 ={num2}");
+        }
+    }
+}
